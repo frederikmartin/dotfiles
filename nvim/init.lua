839600806852d0b7edaf004e72357e75d04d5959
Frederik Martin <frederik.martin@protonmail.com>
2025-06-20 22:44:54 +0200

    chore: update avante claude config

--- nvim/init.lua
+++ nvim/init.lua
@@ -160,11 +160,11 @@ require('lazy').setup({
       providers = {
         claude = {
           endpoint = "https://api.anthropic.com",
-          model = "claude-3-7-sonnet-20250219",
+          model = "claude-sonnet-4-20250514",
           timeout = 30000,
           extra_request_body = {
-            temperature = 0,
-            max_tokens = 4096,
+            temperature = 0.75,
+            max_tokens = 16192,
           },
         },
       },
